3397ed0053b0c00b7ad6195e8c057ea02e820db1
whisky-lover <s3877698@rmit.edu.vn>
2022-08-01 10:58:27 +0700

    loading

--- src/views/OTPPage.js
+++ src/views/OTPPage.js
@@ -7,11 +7,13 @@ import ImgFrame from "../utilities/ImgFrame";
 import NavBar from "../components/header/NavBar";
 import Button from "../utilities/Button";
 import authenticationApi from "../api/authenticationApi";
+import Loading from "../utilities/Loading";
 const OTP_DIGIT_COUNT = 6;
 const OTP_EXPIRE_TIME = 90;
 
 const OTPPage = () => {
 	const [OTP, setOTP] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
 	let { hash, username } = useParams();
 	const navigate = useNavigate();
 	const values = { hash, username, otp: OTP };
@@ -29,6 +31,7 @@ const OTPPage = () => {
 	};
 
 	const handleVerifyClick = () => {
+		setIsLoading((prev) => !prev);
 		verifyOTP();
 	};
 
@@ -41,47 +44,56 @@ const OTPPage = () => {
 			className="page-container"
 		>
 			<NavBar />
-			<div className="flex flex-col space-y-4 lg:text-center">
-				<h3 className="text-2xl font-bold lg:text-5xl md:mb-2 lg:mb-5">
-					Enter your OTP code
-				</h3>
-				<p className="font-thin md:text-xl lg:text-2xl md:font-medium">
-					We have send an 6-digit-OTP code to s3877698@rmit.edu.vn
-				</p>
-			</div>
+			{isLoading ? (
+				<div className="flex items-center justify-center my-10">
+					<Loading />
+				</div>
+			) : (
+				<>
+					<div className="flex flex-col space-y-4 lg:text-center">
+						<h3 className="text-2xl font-bold lg:text-5xl md:mb-2 lg:mb-5">
+							Enter your OTP code
+						</h3>
+						<p className="font-thin md:text-xl lg:text-2xl md:font-medium">
+							We have send an 6-digit-OTP code to
+							s3877698@rmit.edu.vn
+						</p>
+					</div>
 
-			<ImgFrame
-				src={noodle}
-				alt="noodle svg"
-				className="mx-auto md:w-1/2 lg:w-1/3"
-			/>
+					<ImgFrame
+						src={noodle}
+						alt="noodle svg"
+						className="mx-auto md:w-1/2 lg:w-1/3"
+					/>
 
-			<OTPInput
-				value={OTP}
-				onChange={setOTP}
-				autoFocus
-				OTPLength={OTP_DIGIT_COUNT}
-				otpType="number"
-				disabled={false}
-				secure={false}
-				inputClassName="!m-0 text-center min-w-[50px] lg:min-w-[100px] lg:min-h-[100px] md:min-w-[75px] md:min-h-[75px] block text-xl font-bold min-h-[50px] bg-darkRed text-white outline-none input border-none text-white-200 dark:text-black-400 p-0 md:text-2xl lg:text-4xl"
-				className="!grid grid-cols-6 my-8 place-items-center"
-			/>
-			<ResendOTP
-				maxTime={OTP_EXPIRE_TIME}
-				renderTime={renderTime}
-				className="relative flex flex-col"
-				onResendClick={handleResendClick}
-				renderButton={renderButton}
-			/>
+					<OTPInput
+						value={OTP}
+						onChange={setOTP}
+						autoFocus
+						OTPLength={OTP_DIGIT_COUNT}
+						otpType="number"
+						disabled={false}
+						secure={false}
+						inputClassName="!m-0 text-center min-w-[50px] lg:min-w-[100px] lg:min-h-[100px] md:min-w-[75px] md:min-h-[75px] block text-xl font-bold min-h-[50px] bg-darkRed text-white outline-none input border-none text-white-200 dark:text-black-400 p-0 md:text-2xl lg:text-4xl"
+						className="!grid grid-cols-6 my-8 place-items-center"
+					/>
+					<ResendOTP
+						maxTime={OTP_EXPIRE_TIME}
+						renderTime={renderTime}
+						className="relative flex flex-col"
+						onResendClick={handleResendClick}
+						renderButton={renderButton}
+					/>
 
-			<Button
-				content="Verify"
-				onClick={handleVerifyClick}
-				className={`${
-					OTP.length === 6 ? "block" : "hidden"
-				} mt-4 md:text-xl btn w-full transition-all duration-300 md:h-[60px]`}
-			/>
+					<Button
+						content="Verify"
+						onClick={handleVerifyClick}
+						className={`${
+							OTP.length === 6 ? "block" : "hidden"
+						} mt-4 md:text-xl btn w-full transition-all duration-300 md:h-[60px]`}
+					/>
+				</>
+			)}
 		</motion.div>
 	);
 };
