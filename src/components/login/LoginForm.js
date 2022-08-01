505fcaf7f375ab4e29e16745a0ca10b80cd96940
whisky-lover <s3877698@rmit.edu.vn>
2022-08-01 10:54:23 +0700

    loading

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
 import Button from "../../utilities/Button";
 
 import authenticationApi from "../../api/authenticationApi";
+import Loading from "../../utilities/Loading";
 
 const LoginForm = () => {
+	const [isLoading, setIsLoading] = useState(false);
+
 	const navigate = useNavigate();
 
 	const formik = useFormik({
@@ -20,6 +23,7 @@ const LoginForm = () => {
 			const login = async (values) => {
 				authenticationApi.login(values, navigate);
 			};
+			setIsLoading((prev) => !prev);
 
 			login(values);
 		},
@@ -27,42 +31,48 @@ const LoginForm = () => {
 
 	return (
 		<>
-			<form
-				className="w-full mx-auto mt-16 rounded-lg ld:py-6 lg:px-8 lg:w-1/2 md:w-2/3"
-				onSubmit={formik.handleSubmit}
-			>
-				<div className="mb-6 md:mb-10 lg:mb-16">
-					<label
-						htmlFor="username"
-						className="block mb-2 text-sm font-bold uppercase lg:text-lg"
-					>
-						username
-					</label>
-					<input
-						value={formik.values.username}
-						onChange={formik.handleChange}
-						autoComplete="off"
-						type="username"
-						id="username"
-						className="block w-full p-3 text-sm transition-all duration-300 bg-transparent border border-black rounded-lg outline-none dark:border-white input placeholder:text-sm placeholder:text-black border-black-400 dark:border-white-400 md:text-lg dark:placeholder:text-white"
-						placeholder="eg. baonguyen33"
-						required
-					/>
-					{formik.errors.username && (
-						<p
-							className={`my-4 text-sm font-thin text-red-500 md:text-lg transition-all duration-300`}
-						>
-							{formik.errors.username}
-						</p>
-					)}
+			{isLoading ? (
+				<div className="flex items-center justify-center my-10">
+					<Loading />
 				</div>
+			) : (
+				<form
+					className="w-full mx-auto mt-16 rounded-lg ld:py-6 lg:px-8 lg:w-1/2 md:w-2/3"
+					onSubmit={formik.handleSubmit}
+				>
+					<div className="mb-6 md:mb-10 lg:mb-16">
+						<label
+							htmlFor="username"
+							className="block mb-2 text-sm font-bold uppercase lg:text-lg"
+						>
+							username
+						</label>
+						<input
+							value={formik.values.username}
+							onChange={formik.handleChange}
+							autoComplete="off"
+							type="username"
+							id="username"
+							className="block w-full p-3 text-sm transition-all duration-300 bg-transparent border border-black rounded-lg outline-none dark:border-white input placeholder:text-sm placeholder:text-black border-black-400 dark:border-white-400 md:text-lg dark:placeholder:text-white"
+							placeholder="eg. baonguyen33"
+							required
+						/>
+						{formik.errors.username && (
+							<p
+								className={`my-4 text-sm font-thin text-red-500 md:text-lg transition-all duration-300`}
+							>
+								{formik.errors.username}
+							</p>
+						)}
+					</div>
 
-				<Button
-					type="submit"
-					content="Login"
-					className="w-full h-[50px] text-lg mt-10"
-				/>
-			</form>
+					<Button
+						type="submit"
+						content="Login"
+						className="w-full h-[50px] text-lg mt-10"
+					/>
+				</form>
+			)}
 		</>
 	);
 };
--- src/components/main/ProductCard.js
+++ src/components/main/ProductCard.js
@@ -4,7 +4,6 @@ import Button from "../../utilities/Button";
 import star from "../../assets/svg/star.svg";
 
 const ProductCard = ({ starCount, info }) => {
-	console.log("info", info);
 	return (
 		<div className="grid shadow-xl shrink-0 snap-start grid-cols-3 gap-3 md:gap-6 p-2 overflow-hidden rounded-lg place-items-center h-[150px] md:h-[175px] w-full max-w-[350px] mr-3">
 			<ImgFrame
@@ -14,7 +13,9 @@ const ProductCard = ({ starCount, info }) => {
 
 			<div className="flex flex-col items-start justify-start w-full h-full col-span-2 col-start-2">
 				<div className="mb-1">
-					<h3 className="mt-2 font-bold md:text-lg">{"sfdgs"}</h3>
+					<h3 className="mt-2 font-bold md:text-lg">
+						{info.foodName}
+					</h3>
 					<div className="text-[10px] md:text-sm font-normal">
 						<h6>{info.vendor.username}</h6>
 						<p>{info.location}</p>
@@ -35,7 +36,7 @@ const ProductCard = ({ starCount, info }) => {
 				</div>
 
 				<div className="flex items-center justify-between w-full mt-3">
-					<h3 className="font-bold">12</h3>
+					<h3 className="font-bold">{info.price}</h3>
 
 					<Button
 						className="text-[10px] btn-sm"
