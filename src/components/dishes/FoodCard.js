eb9d5ce85520ae4f4a07053e31f4dbcbdb6d33c8
whisky-lover <s3877698@rmit.edu.vn>
2022-08-12 10:14:45 +0700

    food module

--- src/components/dishes/FoodCard.js
+++ src/components/dishes/FoodCard.js
@@ -1,42 +1,46 @@
 import React from "react";
 import star from "../../assets/svg/star.svg";
+import { Link } from "react-router-dom";
 
 const FoodCard = ({ food }) => {
+	console.log(food);
 	return (
-		<div className="w-[300px] h-[400px] shadow-xl rounded-lg p-4">
-			<div className="h-[200px] rounded-lg overflow-hidden">
-				<img
-					src={food.image}
-					alt=""
-					className="object-cover w-full h-full transition-all duration-300"
-				/>
-			</div>
-
-			<div className="flex flex-col items-start justify-center h-[150px] my-4">
-				<div className="flex flex-col flex-1">
-					<h3 className="text-2xl font-bold">{food.foodName}</h3>
-					<h6 className="font-thin capitalize">
-						{food.vendor.username}
-					</h6>
-					<div className="flex items-center justify-start my-2 space-x-1">
-						{Array(5)
-							.fill()
-							.map((_, index) => (
-								<img
-									key={index}
-									src={star}
-									alt=""
-									className="w-4 h-4"
-								/>
-							))}
-					</div>
+		<Link to={`/detail/${food._id}`}>
+			<div className="w-[300px] h-[400px] shadow-xl rounded-lg p-4">
+				<div className="h-[200px] rounded-lg overflow-hidden">
+					<img
+						src={food.image}
+						alt=""
+						className="object-cover w-full h-full transition-all duration-300"
+					/>
 				</div>
 
-				<h3 className="flex text-3xl font-semibold text-red">
-					{food.price}VND
-				</h3>
+				<div className="flex flex-col items-start justify-center h-[150px] my-4">
+					<div className="flex flex-col flex-1">
+						<h3 className="text-2xl font-bold">{food.foodName}</h3>
+						<h6 className="font-thin capitalize">
+							{food.vendor.username}
+						</h6>
+						<div className="flex items-center justify-start my-2 space-x-1">
+							{Array(5)
+								.fill()
+								.map((_, index) => (
+									<img
+										key={index}
+										src={star}
+										alt=""
+										className="w-4 h-4"
+									/>
+								))}
+						</div>
+					</div>
+
+					<h3 className="flex text-3xl font-semibold text-red">
+						{food.price}VND
+					</h3>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
--- src/views/DetailPage.js
+++ src/views/DetailPage.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import NavBar from "../components/header/NavBar";
 import ImgFrame from "../utilities/ImgFrame";
 import Button from "../utilities/Button";
 import Footer from "../components/footer/Footer";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import FoodCard from "../components/dishes/FoodCard";
+import Loading from "../utilities/Loading";
 
 const DetailPage = () => {
+	const { foodId } = useParams();
+	const [food, setFood] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const getFood = async () => {
+			const res = await axios.get(`/api/food/${foodId}`);
+
+			console.log(res.data);
+
+			setFood(res.data.food);
+			setIsLoading(false);
+		};
+
+		getFood();
+	}, [foodId]);
+
+	console.log(food);
+
 	return (
 		<motion.div
 			initial={{ scaleY: 0 }}
@@ -15,57 +38,55 @@ const DetailPage = () => {
 			className="page-container"
 		>
 			<NavBar />
-
-			<ImgFrame
-				className="md:h-[400px] lg:w-[800px] lg:h-[600px] lg:mx-auto rounded-md overflow-hidden mt-5"
-				src="https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
-			/>
-
-			<div className="flex flex-col my-10 space-y-2 lg:my-16">
-				<h1 className="text-2xl font-bold lg:text-4xl">
-					Vietnamese broken rice
-				</h1>
-				<p className="text-lg font-thin md:font-normal lg:text-2xl">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-					Suscipit, rerum! Lorem ipsum dolor sit amet.
-				</p>
-				<Button
-					content="Add to cart"
-					className="w-1/2 md:w-1/3 !mt-6 lg:w-1/6"
-				/>
-			</div>
-
-			<div className="my-10">
-				<h1 className="mb-3 text-2xl font-bold lg:text-4xl">Note</h1>
-				<input
-					type="text"
-					className="w-full input md:text-lg"
-					placeholder="Note to chef. Eg. No spicy"
-				/>
-			</div>
-
-			<div className="my-10">
-				<h1 className="text-2xl font-bold lg:text-4xl">Similar food</h1>
-				<div className="grid grid-cols-3 my-3 lg:my-10">
-					{Array(3)
-						.fill()
-						.map((_, index) => (
-							<div key={index}>
-								<div className="w-[75px] h-[75px] md:w-[150px] md:h-[150px] overflow mx-auto">
-									<img
-										src="https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1380&q=80"
-										alt=""
-										className="object-cover w-full h-full"
-									/>
-								</div>
-
-								<p className="mt-2 text-base font-bold text-center md:mt-4 md:text-2xl">
-									Pancake
-								</p>
-							</div>
-						))}
+			{isLoading ? (
+				<div className="flex items-center justify-center my-6">
+					<Loading />
 				</div>
-			</div>
+			) : (
+				<>
+					<ImgFrame
+						className="md:h-[400px] lg:w-[800px] lg:h-[600px] lg:mx-auto rounded-md overflow-hidden mt-5"
+						src={food?.image}
+					/>
+
+					<div className="flex flex-col my-10 space-y-2 lg:my-16">
+						<h1 className="text-2xl font-bold lg:text-4xl">
+							{food?.foodName}
+						</h1>
+						<p className="text-lg font-thin md:font-normal lg:text-2xl">
+							Lorem ipsum, dolor sit amet consectetur adipisicing
+							elit. Suscipit, rerum! Lorem ipsum dolor sit amet.
+						</p>
+						<Button
+							content="Add to cart"
+							className="w-1/2 md:w-1/3 !mt-6 lg:w-1/6"
+						/>
+					</div>
+
+					<div className="my-10">
+						<h1 className="mb-3 text-2xl font-bold lg:text-4xl">
+							Note
+						</h1>
+						<input
+							type="text"
+							className="w-full input md:text-lg"
+							placeholder="Note to chef. Eg. No spicy"
+						/>
+					</div>
+
+					<div className="my-10">
+						<h1 className="text-2xl font-bold lg:text-4xl">
+							Similar food
+						</h1>
+						<div className="grid grid-cols-3 my-3 lg:my-10">
+							{food?.similarOnes.length > 0 &&
+								food?.similarOnes.map((item, index) => (
+									<FoodCard food={item} key={index} />
+								))}
+						</div>
+					</div>
+				</>
+			)}
 
 			<Footer />
 		</motion.div>
