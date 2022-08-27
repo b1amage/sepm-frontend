797f1961bd26e42326a659a0005a94d1b90cd927
whisky-lover <s3877698@rmit.edu.vn>
2022-08-27 10:06:51 +0700

    create + view all + view one food

--- src/components/dashboard/DashboardModal.js
+++ src/components/dashboard/DashboardModal.js
@@ -1,7 +1,133 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Button from "../../utilities/Button";
 
-const DashboardModal = () => {
-	return <div></div>;
+const DashboardModal = ({ onAdd }) => {
+	const formik = useFormik({
+		initialValues: {
+			foodName: "",
+			location: "",
+			price: 0,
+			category: "",
+			prepareTime: 15,
+			type: "",
+		},
+		validationSchema: Yup.object({
+			foodName: Yup.string().required("Required"),
+			location: Yup.string().required("Required"),
+			price: Yup.number().required("Required"),
+			prepareTime: Yup.number().required("Required"),
+		}),
+		onSubmit: (values) => {
+			onAdd({
+				...values,
+				image: "https://res.cloudinary.com/dxvbvzxcb/image/upload/v1656757372/file-upload/tmp-4-1656757396063_qhmk5m.png",
+			});
+		},
+	});
+
+	return (
+		<div className="p-6">
+			<h3 className="text-2xl font-bold">Add food</h3>
+			<form onSubmit={formik.handleSubmit}>
+				<div>
+					<label className="label" htmlFor="foodName">
+						Food name
+					</label>
+					<input
+						value={formik.values.foodName}
+						onChange={formik.handleChange}
+						type="text"
+						id="foodName"
+						name="foodName"
+						className="w-full input"
+					/>
+				</div>
+
+				<div>
+					<label className="label" htmlFor="location">
+						Location
+					</label>
+					<input
+						value={formik.values.location}
+						onChange={formik.handleChange}
+						type="text"
+						id="location"
+						name="location"
+						className="w-full input"
+					/>
+				</div>
+
+				<div>
+					<label className="label" htmlFor="price">
+						Price
+					</label>
+					<input
+						value={formik.values.price}
+						onChange={formik.handleChange}
+						type="text"
+						id="price"
+						name="price"
+						className="w-full input"
+					/>
+				</div>
+
+				<div>
+					<label className="label" htmlFor="prepareTime">
+						Prepare time
+					</label>
+					<input
+						value={formik.values.prepareTime}
+						onChange={formik.handleChange}
+						type="text"
+						id="prepareTime"
+						name="prepareTime"
+						className="w-full input"
+					/>
+				</div>
+				<div className="flex items-center gap-5">
+					<div>
+						<label className="label" htmlFor="category">
+							Category
+						</label>
+						<select
+							value={formik.values.category}
+							onChange={formik.handleChange}
+							name="category"
+							className="w-full max-w-xs select"
+							id="category"
+						>
+							<option>Soup</option>
+							<option>Rice</option>
+							<option>Bread</option>
+							<option>Noodle</option>
+							<option>Dessert</option>
+						</select>
+					</div>
+
+					<div>
+						<label className="label" htmlFor="type">
+							Type
+						</label>
+						<select
+							value={formik.values.type}
+							onChange={formik.handleChange}
+							name="type"
+							className="w-full max-w-xs select"
+							id="type"
+						>
+							<option>Breakfast</option>
+							<option>Lunch</option>
+							<option>Dinner</option>
+						</select>
+					</div>
+				</div>
+
+				<Button content="submit" className="my-8" />
+			</form>
+		</div>
+	);
 };
 
 export default DashboardModal;
--- src/views/DashBoardPage.js
+++ src/views/DashBoardPage.js
@@ -6,17 +6,74 @@ import axios from "axios";
 import Button from "../utilities/Button";
 import { BsFillPlusCircleFill, BsFillPenFill } from "react-icons/bs";
 import { AiFillDelete, AiFillEye } from "react-icons/ai";
+import DashboardModal from "../components/dashboard/DashboardModal";
+import ImgFrame from "../utilities/ImgFrame";
+import search from "../assets/svg/search.svg";
 
 const DashBoardPage = () => {
 	const [dishes, setDishes] = useState([]);
+	const [nextCursor, setNextCursor] = useState();
+	const [query, setQuery] = useState("");
+	const [showModal, setShowModal] = useState(false);
+
+	const handleAdd = (food) => {
+		const add = async () => {
+			console.log("add");
+
+			const res = await axios.post(`/api/food`, food);
+
+			console.log(res);
+		};
+
+		add();
+	};
+
+	const handleGetOne = () => {
+		const fetch = async () => {
+			const res = await axios.get(`/api/food?foodName=${query}`);
+
+			console.log(res);
+
+			setDishes(res.data.results);
+			setNextCursor(null);
+		};
+
+		fetch();
+	};
+
+	const handleShowMore = () => {
+		if (!nextCursor) return;
+
+		const fetch = async () => {
+			const res = await axios.get(
+				`/api/food/?vendor=global&next_cursor=${nextCursor}`
+			);
+			console.log(res);
+			setDishes([...dishes, ...res.data.results]);
+			setNextCursor(res.data.next_cursor);
+		};
+		fetch();
+	};
 
 	useEffect(() => {
 		const fetch = async () => {
-			const res = await axios.get("/api/food/?vendor=global");
+			const res = await axios.get(`/api/food/?vendor=global`);
+			console.log(res);
 			setDishes(res.data.results);
+			setNextCursor(res.data.next_cursor);
 		};
 		fetch();
-	}, [dishes]);
+	}, []);
+
+	const handleSeeAll = () => {
+		const fetch = async () => {
+			const res = await axios.get(`/api/food/?vendor=global`);
+			console.log(res);
+			setDishes(res.data.results);
+			setNextCursor(res.data.next_cursor);
+		};
+		fetch();
+	};
 
 	return (
 		<div className="page-container">
@@ -25,12 +82,35 @@ const DashBoardPage = () => {
 				<h3 className="my-4 text-3xl font-bold uppercase">
 					Global featured dishes
 				</h3>
+
+				<div className="flex mb-12 md:mb-[30px] items-center h-[50px] justify-between mt-8 space-x-2 lg:text-2xl">
+					<input
+						type="text"
+						onChange={(e) => setQuery(e.target.value)}
+						placeholder="Pick your favorite"
+						className="w-full h-full p-3 border border-black lg:p-6 dark:placeholder:text-white md:text-xl md:placeholder:text-xl lg:text-2xl lg:placeholder:text-2xl dark:border-white placeholder:text-black input"
+					/>
+
+					<div className="cursor-pointer" onClick={handleGetOne}>
+						<ImgFrame
+							src={search}
+							imgClassName="w-6 h-6"
+							className="flex items-center justify-center h-full p-2 rounded-md w-14 bg-red"
+						/>
+					</div>
+				</div>
+
 				<div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 place-items-center">
 					{dishes?.length > 0 &&
 						dishes.map((item, index) => (
 							<FoodCard food={item} key={index} />
 						))}
 				</div>
+				{nextCursor && (
+					<div className="flex items-center justify-center my-8">
+						<Button content="Show more" onClick={handleShowMore} />
+					</div>
+				)}
 			</div>
 			<div className="my-8 lg:my-12">
 				<h3 className="my-4 text-3xl font-bold uppercase">
@@ -38,12 +118,14 @@ const DashBoardPage = () => {
 				</h3>
 				<div className="grid grid-cols-2 gap-5 my-8 lg:grid-cols-4 place-items-center">
 					<Button
+						onClick={() => setShowModal(!showModal)}
 						className="w-[75px] h-[75px] md:h-[120px] md:w-[120px]"
 						content={
 							<BsFillPlusCircleFill className="text-3xl md:text-4xl lg:text-5xl" />
 						}
 					/>
 					<Button
+						onClick={handleSeeAll}
 						className="w-[75px] h-[75px] md:h-[120px] md:w-[120px]"
 						content={
 							<AiFillEye className="text-3xl md:text-4xl lg:text-5xl" />
@@ -64,28 +146,12 @@ const DashBoardPage = () => {
 				</div>
 			</div>
 
+			{showModal && <DashboardModal onAdd={handleAdd} />}
+
 			<div className="my-8">
 				<h3 className="my-4 text-3xl font-bold uppercase">
 					Order food
 				</h3>
-
-				{/* <div className="flex items-center h-full">
-					<div className="w-[120px] h-[120px]">
-						<img
-							src="https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80"
-							alt=""
-						/>
-					</div>
-
-					<div>
-						<h3>Name</h3>
-						<p>description</p>
-						<p>note</p>
-						<p>4</p>
-					</div>
-
-					<div>by user</div>
-				</div> */}
 			</div>
 
 			<Footer />
